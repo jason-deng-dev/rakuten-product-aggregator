82edbb0ae0dc21aacfb7899f167112cf07b2863d
Jason <jason13.deng@gmail.com>
2026-03-16 10:55:08 +0900

    feat: start from scratch

--- db/pools.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = new Pool({
-  host: "localhost", 
-  user: "<role_name>",
-  database: "top_users",
-  password: "<role_password>",
-  port: 5432 
-});
--- routes/indexRouter.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const indexRouter = Router();
-const indexController = require("../controllers/indexController");
-
-
-module.exports = indexRouter;
