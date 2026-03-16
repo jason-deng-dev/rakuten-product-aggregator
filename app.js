051c5c37bead7b50be0c893890f5975e76d93733
Jason <jason13.deng@gmail.com>
2026-03-16 11:07:44 +0900

    feat: additional setup in app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 import 'dotenv/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import express from 'express';
 
 const app = express();
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
